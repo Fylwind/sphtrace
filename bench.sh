cd6ebf89e04318475300d82519f64903ebcf3b1c
Fylwind <fyl@wolfpa.ws>
2015-10-15 02:18:13 -0400

    use extra flags for extra speed

--- bench.sh
+++ bench.sh
@@ -7,5 +7,5 @@ time luajit sphtrace.lua >dist/sphtrace-lua.ppm
 ghc -Wall -O sphtrace.hs -o dist/sphtrace-hs
 time dist/sphtrace-hs
 
-( cd rs && cargo build --release )
+( cd rs && cargo rustc --release -- -C target-cpu=native )
 time rs/target/release/sphtrace >dist/sphtrace-rs.ppm
